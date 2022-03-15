167472a86f3680f58373d0c8d4ec23cd02e6abee
csk6314 <37896060+csk6314@users.noreply.github.com>
2022-03-16 01:39:42 +0900

    bcrypt 1

--- models/User.js
+++ models/User.js
@@ -1,33 +1,52 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        maxlength:50
-    },
-    email : {
-        type:String,
-        trim:true,
-        unique:1
-    },
-    password:{
-        type:String,
-        minlength:5
-    },
-    role:{
-        type:Number,
-        default:0
-    },
-    image:String,
-    token:{
-        type:String,
-    },
-    tokenExp : {
-        type:Number,
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-    }
-})
+userSchema.pre("save", function(next){
+  var user = this;
+  console.log(this);
 
-const User = mongoose.model('User',userSchema);
+  if (user.isModified("password")) {
+    //비번 암호화
 
-module.exports = User;
+    bcrypt.genSalt(saltRounds, function(err, salt)  {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash)  {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
